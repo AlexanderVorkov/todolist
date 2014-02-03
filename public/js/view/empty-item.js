64cec2755806361ca63708e9391214efc644e132
alexander.vorkov <alexander.vorkov@teamdev.com>
2014-02-03 14:51:18 +0200

    moved static files

--- public/js/view/empty-item.js
+++
@@ -1,8 +0,0 @@
-function ViewEmptyItem() {
-    this.templates = _.template($("#emptyListTemplate").text());
-    this.html = $(this.templates());
-}
-
-ViewEmptyItem.prototype.getHtml = function () {
-    return this.html;
-}
